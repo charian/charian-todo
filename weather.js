e87ba16926788b5d2731224ed4417d96386a29fd
charian-laptop <charian@me.com>
2018-11-05 21:28:28 +0900

    commit

--- weather.js
+++ weather.js
@@ -1,9 +1,21 @@
-import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import {LinearGradient} from 'expo';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+import { LinearGradient } from 'expo';
 
 export default class Weather extends Component {
     render() {
+        return(
+            <LinearGradient 
+                colors={['#19182E', '#2D2A6A']} 
+                style={styles.container} 
+            />
+        );
         
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+});
