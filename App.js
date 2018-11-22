3cbb629216b0b33f72c90a1eb9e56a3066534d76
charian-laptop <charian@me.com>
2018-11-22 09:01:49 +0900

    많이 바뀜

--- App.js
+++ App.js
@@ -1,38 +1,295 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image, ActivityIndicator } from 'react-native';
-import Weather from './Weather';
+import React, { Component } from "react";
+import {AppRegistry, StyleSheet, Text, View, StatusBar, Image, TouchableOpacity} from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import Modal from "react-native-modal";
+import SideMenu from 'react-native-side-menu';
 
-export default class App extends Component {
+import Weather from "./Weather";
+import Menu from './Menu';
+
+
+const API_KEY = "T4pjG2TFl6rGlAhzoYeo3bbLEpkmzeiF";
+const datagokr_KEY = "aiMmkue91q4efKfTD0%2FfGF7Zv%2FIFyFZLqLVxcVrI7O9ZEgiKsA1yawel2kBvXgBvwaIAZ7ygUg%2BFJN1ex7n65w%3D%3D";
+const kakao_KEY = "d114ff2a6ca1e7124eb497fbfcb660a4"
+const token = "b8e396fd8d70334cc7860ccc70ae8ee2bbe42d07"
+
+//seoul sample     	cc5o58o52jem0klr43e0le9ubv
+//Key: 226081
+//lat: 37.537
+//long:126.97
+
+export default class App extends Component{
+
   state = {
-    isLoaded: true
-  }
-  render() {
-    const {isLoaded} = this.state;
-    return (
-      <View style={styles.container}>
-        {isLoaded ? <Weather /> : <View style={styles.loading}>
-          <Text style={styles.loadingText}>Getting the SUPER Cool Weather</Text>
-        </View>}
-      </View>
+    isLoaded: false,
+    error: null,
+    isModalVisible: false,
+    menuOpen: false,
+    locationKey: null,
+    temperature: null,
+    name: null,
+    countrytext: null,
+    locationtext: null,
+    thenyesterday: null,
+    humidity: null,
+    cityname: null,
+    mtx: null,
+    mty: null,
+    pmStation: null,
+    currentPositionPM25: null,
+    currentPositionPM10: null
+  };
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this._getWeather(position.coords.latitude, position.coords.longitude);
+        console.log(position);
+      },
+      error => {
+        this.setState({
+          error: error
+        });
+      }
     );
   }
+  _getWeather = (lat, long) => {
+    //fetch(`https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${API_KEY}&q=40.730610,-73.935242`)
+    fetch(`https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${API_KEY}&q=${lat},${long}`)
+      .then(response => response.json())
+      .then(location => {
+        console.log('location data');
+        console.log(location);
+        this.setState({
+          locationKey: location.Key,
+          countrytext: location.Country.LocalizedName,
+          locationtext: location.AdministrativeArea.LocalizedName,
+          cityname: location.ParentCity.LocalizedName
+          //isLoaded: true
+        })
+        return fetch(`https://dataservice.accuweather.com/currentconditions/v1/` + this.state.locationKey + `?apikey=${API_KEY}&details=true`)
+      })
+      .then(response => response.json())
+      .then(locationData => {
+        console.log('weather data');
+        console.log(locationData);
+        this.setState({
+          temperature: locationData[0].Temperature.Metric.Value,
+          name: locationData[0].WeatherText.replace(/\s/gi,""), //.replace(/\-/g,'')
+          thenyesterday: locationData[0].Past24HourTemperatureDeparture.Metric.Value,
+          humidity: locationData[0].RelativeHumidity,
+          isLoaded: true
+        })
+
+        // return fetch(`https://api.waqi.info/feed/geo:${lat};${long}/?token=${token}`)
+        // .then(response => response.json())
+        // .then(AQI => {
+        //   console.log(AQI);
+        // })
+
+
+
+        //return fetch(`https://dapi.kakao.com/v2/local/geo/transcoord.json?x=-73.935242&y=40.730610&input_coord=WGS84&output_coord=TM`,{
+        return fetch(`https://dapi.kakao.com/v2/local/geo/transcoord.json?x=${long}&y=${lat}&input_coord=WGS84&output_coord=TM`,{
+          headers: new Headers({'Authorization': 'KakaoAK d114ff2a6ca1e7124eb497fbfcb660a4'}),
+        })
+        console.log(locationData[0].WeatherText);
+      })
+      .then(response => response.json())
+      .then(wgsTotm => {
+        console.log(wgsTotm);
+        this.setState({
+          mtx: wgsTotm.documents[0].x,
+          mty: wgsTotm.documents[0].y
+        })
+        console.log(wgsTotm.documents[0].x);
+        console.log(wgsTotm.documents[0].y);
+        return fetch(`http://openapi.airkorea.or.kr/openapi/services/rest/MsrstnInfoInqireSvc/getNearbyMsrstnList?tmX=` + this.state.mtx + `&tmY=` + this.state.mty + `&pageNo=1&numOfRows=10&ServiceKey=${datagokr_KEY}&_returnType=json`)
+      })
+      .then(response => response.json())
+      .then(airkoreadata => {
+        console.log(airkoreadata);
+        this.setState({
+          pmStation: airkoreadata.list[0].stationName
+        })
+        console.log(airkoreadata.list[0].stationName);
+        return fetch(`http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=` +  encodeURI(this.state.pmStation , "UTF-8") + `&dataTerm=month&pageNo=1&numOfRows=10&ServiceKey=${datagokr_KEY}&ver=1.3&_returnType=json`)
+        .then(response => response.json())
+        .then(airpolution => {
+          console.log(airpolution);
+          this.setState({
+            currentPositionPM25:airpolution.list[0].pm25Value,
+            currentPositionPM10:airpolution.list[0].pm10Value
+          })
+          console.log(airpolution.list[0].pm25Value);
+          console.log(airpolution.list[0].pm10Value);
+        })
+
+      })
+  };
+
+  //http://openapi.airkorea.or.kr/openapi/services/rest/MsrstnInfoInqireSvc/getNearbyMsrstnList?tmX=197278.84303036687&tmY=448305.79505367856&pageNo=1&numOfRows=10&ServiceKey=aiMmkue91q4efKfTD0%2FfGF7Zv%2FIFyFZLqLVxcVrI7O9ZEgiKsA1yawel2kBvXgBvwaIAZ7ygUg%2BFJN1ex7n65w%3D%3D&_returnType=json
+  //http://openapi.airkorea.or.kr/openapi/services/rest/MsrstnInfoInqireSvc/getNearbyMsrstnList?tmX=244148.546388&tmY=412423.75772&pageNo=1&numOfRows=10&ServiceKey=aiMmkue91q4efKfTD0%2FfGF7Zv%2FIFyFZLqLVxcVrI7O9ZEgiKsA1yawel2kBvXgBvwaIAZ7ygUg%2BFJN1ex7n65w%3D%3D&ver=1.0
+  //http://openapi.airkorea.or.kr/openapi/services/rest/MsrstnInfoInqireSvc/getNearbyMsrstnList?tmX=244148.546388&tmY=412423.75772&pageNo=1&numOfRows=10&ServiceKey=aiMmkue91q4efKfTD0/fGF7Zv/IFyFZLqLVxcVrI7O9ZEgiKsA1yawel2kBvXgBvwaIAZ7ygUg+FJN1ex7n65w==
+
+
+  _toggleModal = () => this.setState({ isModalVisible: !this.state.isModalVisible });
+  
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+
+    this.state = {
+      isOpen: false,
+      selectedItem: 'About',
+    };
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+
+  updateMenuState(isOpen) {
+    this.setState({ isOpen });
+  }
+
+  onMenuItemSelected = item =>
+  this.setState({
+    isOpen: false,
+    selectedItem: item,
+  });
+
+  render() {
+    
+    const { isLoaded, error, temperature, name, countrytext, locationtext, thenyesterday, humidity, cityname, currentPositionPM10, currentPositionPM25} = this.state;
+    const menu = <Menu onItemSelected={this.onMenuItemSelected} />;
+    return (
+      
+      <SideMenu
+        menu={menu}
+        isOpen={this.state.isOpen}
+        onChange={isOpen => this.updateMenuState(isOpen)}
+        style={styles.sidemenu}
+      >
+
+        <View style={styles.container}>
+
+          <View style={styles.headers}>
+            <TouchableOpacity onPress={this.toggle} style={styles.button}>
+              <Image style={styles.callLeftmenu} source={require('./assets/images/hamburger-2x.png')}/>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this._toggleModal} style={styles.locationTitle}>
+              <Text style={styles.locationTitleText}>{this.state.cityname}</Text>
+              <MaterialCommunityIcons name="menu-down" size={32} color="white" style={styles.locationCallIcon} />
+            </TouchableOpacity>
+          </View>
+
+          <Modal 
+          isVisible={this.state.isModalVisible} 
+          animationIn='bounceIn'
+          easing='ease-in'
+          animationOut='fadeOut' 
+          backdropOpacity={0.3}
+          >
+            <View style={styles.modalContainer}>
+              <Text>{`This is the modal!`}</Text>
+              <TouchableOpacity onPress={this._toggleModal}>
+                <Text>Hide me!</Text>
+              </TouchableOpacity>
+            </View>
+          </Modal>
+
+          {isLoaded ? (
+            <Weather
+              weatherName={name}
+              temp={Math.ceil(temperature - 0)}
+              countryText={countrytext}
+              locationText={locationtext}
+              cityName={cityname}
+              thenYesterday={thenyesterday}
+              Humidity={humidity}
+              PM10={currentPositionPM10}
+              PM25={currentPositionPM25}
+            />
+          ) : (
+            <View style={styles.loading}>
+              <Text style={styles.loadingText}>Getting the fucking weather</Text>
+              {error ? <Text style={styles.errorText}>{error}</Text> : null}
+            </View>
+          )}
+        </View>
+      </SideMenu>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
+  sidemenu: {
+    flex: 1,
+    
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff'
+
   },
   loading: {
     flex: 1,
     backgroundColor: '#181740',
     justifyContent: "flex-end",
     paddingLeft: 20,
-    paddingRight: 20
+    paddingRight: 20,
+    zIndex: 1000
   },
   loadingText: {
     fontSize: 38,
     marginBottom: 150,
     color: '#fff'
+  },
+  locationTitle: {
+    alignItems: 'stretch',
+    marginLeft: 15,
+    flexDirection: 'row',
+  },
+  locationTitleText: {
+    fontSize: 27,
+    color: '#fff',
+    fontFamily: "Arial Rounded MT Bold",
+    marginTop: -6
+  },
+  locationCallIcon: {
+    marginTop: -3
+  },
+  callLeftmenu: {
+    width: 24,
+    height: 20,
+    
+  },
+  headers: {
+    flex: 1,
+    flexDirection: 'row',
+    position: 'absolute',
+    top: 67,
+    left: 26,
+    zIndex: 100,
+  },
+  modalLocaton: {
+    zIndex: 150,
+    backgroundColor: '#fff'
+  },
+  absolute: {
+    position: "absolute",
+    top: 0, left: 0, bottom: 0, right: 0,
+  },
+  modalContainer: {
+    backgroundColor: '#fff',
+    elevation:4,
+    shadowOffset: { width: 5, height: 5 },
+    shadowColor: "black",
+    shadowOpacity: 0.5,
+    shadowRadius: 10,
   }
 });
--- weather.js
+++ weather.js
@@ -1,21 +1,204 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View} from 'react-native';
-import { LinearGradient } from 'expo';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { LinearGradient } from "expo";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import PropTypes from "prop-types";
 
-export default class Weather extends Component {
-    render() {
-        return(
-            <LinearGradient 
-                colors={['#19182E', '#2D2A6A']} 
-                style={styles.container} 
-            />
-        );
-        
-    }
+const weatherCases = {
+  Lightrain: {
+    colors: ["#00C6FB", "#005BEA"],
+    title: "Light Raining",
+    subtitle: "For more info look outside",
+    icon: "weather-rainy"
+    },
+  Rain: {
+    colors: ["#00C6FB", "#005BEA"],
+    title: "Raining like a MF",
+    subtitle: "For more info look outside",
+    icon: "weather-rainy"
+  },
+  Clear: {
+    colors: ["#FEF253", "#FF7300"],
+    title: "Sunny as fuck",
+    subtitle: "Go get your ass burnt",
+    icon: "weather-sunny"
+  },
+  Sunny: {
+    colors: ["#FEF253", "#FF7300"],
+    title: "Sunny as fuck",
+    subtitle: "Go get your ass burnt",
+    icon: "weather-sunny"
+  },
+  Partlysunny: {
+    colors: ["#FEF253", "#FF7300"],
+    title: "Sunny as fuck",
+    subtitle: "Go get your ass burnt",
+    icon: "weather-sunny"
+  },
+  Mostlysunny: {
+    colors: ["#FEF253", "#FF7300"],
+    title: "Sunny as fuck",
+    subtitle: "Go get your ass burnt",
+    icon: "weather-sunny"
+  },
+  Mostlyclear: {
+    colors: ["#FEF253", "#FF7300"],
+    title: "Mostly Clear",
+    subtitle: "Go get your ass burnt",
+    icon: "weather-sunny"
+  },
+  Thunderstorm: {
+    colors: ["#00ECBC", "#007ADF"],
+    title: "Thunderstorm in the house",
+    subtitle: "Actually, outside of the house",
+    icon: "weather-lightning"
+  },
+  Clouds: {
+    colors: ["#D7D2CC", "#304352"],
+    title: "Clouds",
+    subtitle: "I know, fucking boring",
+    icon: "weather-cloudy"
+  },
+  Cloudsandsun: {
+    colors: ["#D7D2CC", "#304352"],
+    title: "Partly cloudy",
+    subtitle: "I know, fucking boring",
+    icon: "weather-cloudy"
+  },
+  Partlycloudy: {
+    colors: ["#D7D2CC", "#304352"],
+    title: "Partly cloudy",
+    subtitle: "I know, fucking boring",
+    icon: "weather-cloudy"
+  },
+  Mostlycloudy: {
+    colors: ["#D7D2CC", "#304352"],
+    title: "Mostly cloudy",
+    subtitle: "I know, fucking boring",
+    icon: "weather-cloudy"
+  },
+  Cloudy: {
+    colors: ["#BEBEBE", "#7C8EB6", "#6F6F6F"],
+    title: "Clouds",
+    subtitle: "I know, fucking boring",
+    icon: "weather-cloudy"
+  },
+  Someclouds: {
+    colors: ["#D7D2CC", "#304352"],
+    title: "Some Clouds",
+    subtitle: "I know, fucking boring",
+    icon: "weather-cloudy"
+  },
+  Snow: {
+    colors: ["#7DE2FC", "#B9B6E5"],
+    title: "Cold as balls",
+    subtitle: "Do you want to build a snowman? Fuck no.",
+    icon: "weather-snowy"
+  },
+  Drizzle: {
+    colors: ["#89F7FE", "#66A6FF"],
+    title: "Drizzle",
+    subtitle: "Is like rain, but gay 🏳️‍🌈",
+    icon: "weather-hail"
+  },
+  Haze: {
+    colors: ["#89F7FE", "#66A6FF"],
+    title: "Haze",
+    subtitle: "Don't know what that is 💩",
+    icon: "weather-hail"
+  },
+  Mist: {
+    colors: ["#D7D2CC", "#304352"],
+    title: "Mist!",
+    subtitle: "It's like you have no glasses on.",
+    icon: "weather-fog"
+  },
+  Ashower: {
+    colors: ["#D7D2CC", "#304352"],
+    title: "Mist!",
+    subtitle: "It's like you have no glasses on.",
+    icon: "weather-fog"
+  }
+};
+
+
+
+function Weather({ weatherName, temp ,countryText, locationText, thenYesterday, Humidity, cityName, PM10, PM25}) {
+  return (
+    <LinearGradient
+      colors={weatherCases[weatherName].colors}
+      style={styles.container}
+      start={[0.4, -0.4]}
+      end={[-0.3, 1]}
+      location={[0.25, 0.4, 1]}
+      
+    >
+      <View style={styles.upper}>
+        <MaterialCommunityIcons
+          color="white"
+          size={144}
+          name={weatherCases[weatherName].icon}
+        />
+        <Text style={styles.temp}>{temp}º</Text>
+        <Text>{thenYesterday}º then yesterday</Text>
+      </View>
+      <View style={styles.lower}>
+        <Text>{cityName}, {locationText}, {countryText}</Text>
+        <Text>습도  : {Humidity}%</Text>
+        <Text>한국 데이터 / 초미세먼지 : {PM25}, 미세먼지 : {PM10}</Text>
+        <Text style={styles.title}>{weatherCases[weatherName].title}</Text>
+        <Text style={styles.subtitle}>
+          {weatherCases[weatherName].subtitle}
+        </Text>
+      </View>
+    </LinearGradient>
+  );
 }
+
+Weather.propTypes = {
+  temp: PropTypes.number.isRequired,
+  weatherName: PropTypes.string.isRequired,
+  countryText: PropTypes.string.isRequired,
+  locationText: PropTypes.string.isRequired,
+  thenYesterday: PropTypes.number.isRequired,
+  Humidity: PropTypes.number.isRequired,
+  cityName: PropTypes.string.isRequired,
+  PM10: PropTypes.string,
+  PM25: PropTypes.string
+};
+
+export default Weather;
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        
+    },
+    upper: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    lower: {
+        flex: 1,
+        alignItems: "flex-start",
+        justifyContent: "flex-end",
+        paddingLeft: 25
+    },
+    title: {
+        fontSize: 38,
+        color: '#fff',
+        marginBottom: 10,
+        fontWeight: '200'
+    },
+    subtitle: {
+        fontSize: 24,
+        color: '#fff',
+        marginBottom: 80
+    },
+    temp: {
+        fontSize: 50,
+        color: '#fff',
+        fontWeight: '200'
     }
 });
